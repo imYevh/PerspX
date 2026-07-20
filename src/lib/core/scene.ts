fe75aa2528218d50b210dc91efac41460b40d46e
Yevhenii Petrenko <evgen151218@gmail.com>
2026-07-20 17:58:49 +0300

    Fix syntax error in scene.ts

--- src/lib/core/scene.ts
+++ src/lib/core/scene.ts
@@ -33,6 +33,7 @@ export class SceneManager {
     this.scene = scene;
     if (this.raycaster.params.Line) {
       this.raycaster.params.Line.threshold = 0.05; // Make line selection much more precise
+    }
   }
 
   public updateSelectionBoxes() {
